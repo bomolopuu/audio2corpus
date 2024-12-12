006648ee107dd4cdfbb172ecb7d5aebd56e99db9
razif haque <razif.haque@gmail.com>
2024-12-12 19:56:39 +0600

    updated api

--- api/fast.py
+++ api/fast.py
@@ -7,50 +7,54 @@ import torch
 app = FastAPI()
 
 #load model
-processor = AutoProcessor.from_pretrained("mms-meta/mms-zeroshot-300m")
-model = AutoModelForCTC.from_pretrained("mms-meta/mms-zeroshot-300m")
+# processor = AutoProcessor.from_pretrained("mms-meta/mms-zeroshot-300m")
+# model = AutoModelForCTC.from_pretrained("mms-meta/mms-zeroshot-300m")
 # app.state.model = model
 
 @app.post("/transcribe/")
 async def transcribe(audio_file: UploadFile = File(...),
-                     csv_file: UploadFile = File(...)
+                     vocab_file: UploadFile = File(...)
                      ):
 
-    # Save the uploaded file locally
-    file_location = f"temp_{audio_file.filename}"
-    with open(file_location, "wb") as f:
+    # Save the uploaded audio file
+    audio_path = f"temp_{audio_file.filename}"
+    with open(audio_path, "wb") as f:
         f.write(await audio_file.read())
 
-    # Save the uploaded csv_file locally
-    csv_location = f"temp_{csv_file.filename}"
-    with open(csv_location, "wb") as f:
-        f.write(await csv_file.read())
+    # Save the uploaded vocabulary file
+    vocab_path = f"temp_{vocab_file.filename}"
+    with open(vocab_path, "wb") as f:
+        f.write(await vocab_file.read())
 
-    # # Load audio using librosa
-    # waveform, sample_rate = librosa.load(file_location, sr=16000, mono=True)
+    # # Load the audio
+    # waveform, sample_rate = librosa.load(audio_path, sr=16000, mono=True)
 
-    # # Convert waveform to tensor
-    # waveform_tensor = torch.tensor(waveform).unsqueeze(0)
+    # #preprocess the audio
+    # inputs = processor(waveform, sampling_rate=16000, return_tensors="pt", padding=True)
 
-    # Preprocess the audio
-    # inputs = processor(waveform, sampling_rate=sample_rate, return_tensors="pt", padding=True)
 
-    # Generate transcription logits
+    # # Device Management
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    # model.to(device)
+    # inputs = inputs.to(device)
+
+    # # Perform transcription
     # with torch.no_grad():
-    #     logits = model(inputs.input_values).logits
+    #         logits = model(inputs.input_values).logits
 
-    # Decode the logits to text
+    # # Decode the logits to transcription
     # predicted_ids = torch.argmax(logits, dim=-1)
     # transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
 
+
     # Clean up the temporary file
-    os.remove(file_location)
-    os.remove(csv_location)
+    os.remove(audio_path)
+    os.remove(vocab_path)
 
     # return {"transcription": transcription}
 
     return {"audio": {audio_file.filename},
-            "vocabulary": {csv_file.filename}}
+            "vocabulary": {vocab_file.filename}}
 
 
 @app.get("/")
