d3b7e1d16a8c42f9fd34fb72ce749a687a136763
razif haque <razif.haque@gmail.com>
2024-12-12 21:09:07 +0600

    api updated

--- api/fast.py
+++ api/fast.py
@@ -7,9 +7,9 @@ import torch
 app = FastAPI()
 
 #load model
-# processor = AutoProcessor.from_pretrained("mms-meta/mms-zeroshot-300m")
-# model = AutoModelForCTC.from_pretrained("mms-meta/mms-zeroshot-300m")
-# app.state.model = model
+processor = AutoProcessor.from_pretrained("mms-meta/mms-zeroshot-300m")
+model = AutoModelForCTC.from_pretrained("mms-meta/mms-zeroshot-300m")
+app.state.model = model
 
 @app.post("/transcribe/")
 async def transcribe(audio_file: UploadFile = File(...),
@@ -26,34 +26,35 @@ async def transcribe(audio_file: UploadFile = File(...),
     with open(vocab_path, "wb") as f:
         f.write(await vocab_file.read())
 
-    # # Load the audio
-    # waveform, sample_rate = librosa.load(audio_path, sr=16000, mono=True)
+    # Load the audio
+    waveform, sample_rate = librosa.load(audio_path, sr=16000, mono=True)
 
-    # #preprocess the audio
-    # inputs = processor(waveform, sampling_rate=16000, return_tensors="pt", padding=True)
+    #preprocess the audio
+    inputs = processor(waveform, sampling_rate=16000, return_tensors="pt", padding=True)
 
+    model=app.state.model
 
-    # # Device Management
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    # model.to(device)
-    # inputs = inputs.to(device)
+    # Device Management
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    inputs = inputs.to(device)
 
-    # # Perform transcription
-    # with torch.no_grad():
-    #         logits = model(inputs.input_values).logits
+    # Perform transcription
+    with torch.no_grad():
+            logits = model(inputs.input_values).logits
 
-    # # Decode the logits to transcription
-    # predicted_ids = torch.argmax(logits, dim=-1)
-    # transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+    # Decode the logits to transcription
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
 
     # Clean up the temporary file
     os.remove(audio_path)
     os.remove(vocab_path)
 
-    # return {"transcription": transcription}
+    return {"transcription": transcription}
 
-    return {"audio": {audio_file.filename},
-            "vocabulary": {vocab_file.filename}}
+    # return {"audio": {audio_file.filename},
+    #         "vocabulary": {vocab_file.filename}}
 
 
 @app.get("/")
